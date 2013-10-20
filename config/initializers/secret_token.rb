9c78daabd6b9af2a9bb514acc9eeee217a9bfa68
Ali Altaf <alialtaf@seas.upenn.edu>
2013-10-19 20:01:15 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clear4Asana::Application.config.secret_token = '4e1508739a351194ae003a4baf38b52470ef6cac53c33f507a617674ce3418fe75957a05aa3c74f5cb1acb096a7e92d19f12c1273b6fa347d04311689d0652d3'
